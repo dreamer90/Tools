e0354a9c687fb2c928fd9c2ecf1801d782cd0519
lidi <dreamerjl@163.com>
2016-12-29 10:07:15 +0800

    初始化项目

---
+++ Tools/Tools/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tools
+//
+//  Created by hyfsoft on 2016/12/29.
+//  Copyright © 2016年 hyfsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Tools/Tools/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tools
+//
+//  Created by hyfsoft on 2016/12/29.
+//  Copyright © 2016年 hyfsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
